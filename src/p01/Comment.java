321de60cb196accbac1941d14f20ee08091016f8
keomyeong <ljn0843@naver.com>
2021-03-25 11:33:24 +0900

    chapter 01

--- src/p01/Comment.java
+++ src/p01/Comment.java
@@ -5,7 +5,7 @@ public class Comment {
 		System.out.println("주석");
 		
 		// 한줄 주석은 slash 두개
-		System.out.println("한 줄 주석");
+		System.out.println("한 줄 주석"); 
 		
 		/*
 		 * 여러줄 주석은 slash 별 ~ 별 slash 까지
